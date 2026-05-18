c6fcbc6b45689a72ed055ac7b2a9d8311c40fd0d
Your Name <you@example.com>
2026-05-18 02:28:29 +0000

    new try

--- graph.c
+++ graph.c
@@ -66,14 +66,12 @@ List* getEdges(Graph* g, const char* label) {
 
 int getWeight(Graph* g, const char* label1, const char* label2) {
     if (!g || !label1 || !label2) return -1;
-
     List* edgesList = (List*)map_search(g->adjacencyMap, (void*)label1);
     if (!edgesList) return -1;
-
     Edge* e = (Edge*)list_first(edgesList);
-    while (e != NULL) {
-        if (e->target && strcmp(e->target, label2) == 0) {
-            return e->weight; 
+    while (e != NULL){
+        if (strcmp(e->target, label2) == 0){
+            return e->weight;
         }
         e = (Edge*)list_next(edgesList);
     }
